8c5757b38ac44c855d3238f03eaf646d2e40e74e
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-05-20 13:06:19 +0600

    Update start.py

--- commands/start.py
+++ commands/start.py
@@ -10,3 +10,68 @@ from commands.start import start
 
 # Register the start command handler
 dispatcher.add_handler(CommandHandler('start', start))
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import CommandHandler, CallbackContext
+
+# Define the start command handler function
+def start(update: Update, context: CallbackContext):
+    # Get the user's chat ID and username
+    chat_id = update.effective_chat.id
+    username = update.effective_user.username
+
+    # Perform human verification (e.g., CAPTCHA)
+    # Your implementation for human verification goes here
+
+    # If the user passed the verification
+    if passed_verification:
+        # Welcome the user with a mention and prompt to join your channel
+        welcome_message = f"Hello @{username}! Welcome to the Bot! 🎉\n\nPlease join our channel to access exclusive content:"
+
+        # Generate random text with emoji for prompting to join the channel
+        join_texts = [
+            "Join Now Otherwise The Bot Gets Banned! 🚫",
+            "Don't Miss Out! Join Now! 🔥",
+            "Join our Channel for Updates! 📢",
+            "Get Exclusive Access by Joining Now! 💪",
+            "Join and Stay Informed! 🌟",
+            # Add more join texts with emoji as desired
+        ]
+
+        # Create an inline keyboard with the join button
+        join_button = InlineKeyboardButton(text="Join Now", url="https://t.me/your_channel_link")
+        keyboard = InlineKeyboardMarkup([[join_button]])
+
+        # Send the welcome message with the join prompt
+        context.bot.send_message(chat_id=chat_id, text=welcome_message, reply_markup=keyboard)
+
+        # Send multiple messages with different join texts every 5 seconds
+        for i, join_text in enumerate(join_texts):
+            # Add random emoji to the join text
+            join_text_with_emoji = f"{join_text} {random.choice(emoji_list)}"
+
+            # Send the join text message
+            context.bot.send_message(chat_id=chat_id, text=join_text_with_emoji)
+
+            # Sleep for 5 seconds before sending the next join text
+            time.sleep(5)
+
+        # Force user to join the group
+        force_join_message = f"⚠️ Hey @{username}! You must join our group to proceed. Click the button below to join:"
+        force_join_button = InlineKeyboardButton(text="Join Now", url="https://t.me/joinchat/your_group_link")
+        force_join_keyboard = InlineKeyboardMarkup([[force_join_button]])
+
+        # Send the force join message
+        context.bot.send_message(chat_id=chat_id, text=force_join_message, reply_markup=force_join_keyboard)
+
+        # Send a good welcome message after the force join
+        final_welcome_message = f"Thank you for joining our group, @{username}! We're glad to have you on board. Enjoy your experience with the Bot! 🙌"
+        context.bot.send_message(chat_id=chat_id, text=final_welcome_message)
+
+    else:
+        # Handle the case if the user failed the human verification
+        verification_failed_message = "Oops! It seems you failed the human verification. Please try again later."
+        context.bot.send_message(chat_id=chat_id, text=verification_failed_message)
+
+
+# Register the start command handler
+dispatcher.add_handler(CommandHandler('start',
